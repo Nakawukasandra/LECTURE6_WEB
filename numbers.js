dd8cc802f3560ba6507cca7e4cd6aa29386b9f50
Nakawukasandra <nakawukasandra8@gmail.com>
2024-04-02 23:48:25 +0300

    LECTURE6

---
+++ numbers.js
@@ -0,0 +1,14 @@
+//to string()method
+let x = 123;
+x.toString();
+console.log(x.toString())
+
+
+//The toExponential() Method
+let y = 9.656;
+console.log(y.toExponential(2))
+
+//the toFixed()method
+console.log(y.toFixed(2))
+
+//The toPrecision() Method
